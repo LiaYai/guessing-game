c91147e09504bbf3b2c82febb04775f4d56ee4e8
Talissa <liayai@ya.ru>
2024-08-30 16:32:57 +0300

    end

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = 0;
+    }
+  
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+  
     guess() {
-
+      if (this.result === this.max) {
+        this.result = Math.ceil(this.result - (this.max - this.min) / 2);
+      } else {
+        this.result = Math.ceil(this.result + (this.max - this.min) / 2);
+      }
+      return this.result;
     }
-
+  
     lower() {
-
+      this.setRange(this.min, this.result);
     }
-
+  
     greater() {
-
+      this.setRange(this.result, this.max);
     }
-}
+  }
 
 module.exports = GuessingGame;
